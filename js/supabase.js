19d08ccad7246cd85889b8f38fb16b39a7467828
krishkakadiya1602 <kakadiyakrish90@gmail.com>
2026-09-19 14:59:47 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,11 @@
-const SUPABASE_URL =
+const SUPABASE_URL = 
     "https://xqcfhpzilpkjqgthskh.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY =
-    "sb_publishable_N07LrmX12l8ARqQzBCLIow_d2Ght8rO";
+const SUPABASE_ANON_KEY = 
+    "supabase_anon_key_N07LrmX12l8ARqQzBCLIow_d2Ght8rO";
 
-const supabaseClient =
-    window.supabase.createClient(
-        SUPABASE_URL,
-        SUPABASE_PUBLISHABLE_KEY
-    );
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
