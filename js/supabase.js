d9208a601ad1e064dde6a52f1080019cfcd6b45f
krishkakadiya1602 <kakadiyakrish90@gmail.com>
2026-09-19 13:15:43 +0530

    Update Supabase publishable key

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
     "https://xqcfhpzilpkjqgthskh.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "YOUR_SB_PUBLISHABLE_KEY";
+    "sb_publishable_N07LrmX12l8ARqQzBCLIow_d2Ght8rO";
 
 const supabaseClient =
     window.supabase.createClient(
