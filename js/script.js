fc6954b961b252f729541d4afba666ad48d9d4cb
krishkakadiya1602 <kakadiyakrish90@gmail.com>
2026-09-19 12:25:29 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://xqcfhpzilpkjqgthskh.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_xxxxxxxxxxxxx";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
