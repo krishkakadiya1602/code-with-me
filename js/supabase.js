357681a1cec729b34aa943100663c46f1ab57d75
krishkakadiya1602 <kakadiyakrish90@gmail.com>
2026-09-21 18:12:42 +0530

    Remove leading space from SUPABASE_ANON_KEY

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
     "https://voecwxorcxothdfhaged.supabase.co";
 
 const SUPABASE_ANON_KEY =
-    " sb_publishable_zCzBS1o-NxP6KyZ7JCT53g_1qRd5reQ";
+    "sb_publishable_zCzBS1o-NxP6KyZ7JCT53g_1qRd5reQ";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
