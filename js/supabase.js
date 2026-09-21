7fb16823da64bbded48cf90c5e7329cb6c376a77
krishkakadiya1602 <kakadiyakrish90@gmail.com>
2026-09-21 15:04:47 +0530

    Initialize Supabase client with URL and anon key

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL =
+    "https://voecwxorcxothdfhaged.supabase.co";
+
+const SUPABASE_ANON_KEY =
+    " sb_publishable_zCzBS1o-NxP6KyZ7JCT53g_1qRd5reQ";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
