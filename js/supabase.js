275f58f08d80f87853709fae836c708cc5ec677a
krishkakadiya1602 <kakadiyakrish90@gmail.com>
2026-09-22 14:55:30 +0530

    Update Supabase client configuration logic
    Refactor Supabase client initialization to check for valid URL and key.

--- js/supabase.js
+++ js/supabase.js
@@ -1,10 +1,26 @@
+// ========================================
+// Code With Me - Supabase Configuration
+// ========================================
+
 const SUPABASE_URL =
-    "https://voecwxorcxothdfhaged.supabase.co";
+    "https://voecwxorcxothdfhaged.supabase.co/";
 
 const SUPABASE_ANON_KEY =
     "sb_publishable_zCzBS1o-NxP6KyZ7JCT53g_1qRd5reQ";
 
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
+
+let supabaseClient = null;
+
+
+if (
+    SUPABASE_URL !== "YOUR_SUPABASE_URL" &&
+    SUPABASE_ANON_KEY !== "YOUR_SUPABASE_ANON_KEY"
+) {
+
+    supabaseClient =
+        window.supabase.createClient(
+            SUPABASE_URL,
+            SUPABASE_ANON_KEY
+        );
+
+}
