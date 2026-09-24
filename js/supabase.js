9b015967882aeda2907669d076307962adaf8f29
krishkakadiya1602 <kakadiyakrish90@gmail.com>
2026-09-24 13:49:51 +0530

    Clean up supabase.js by removing unused code
    Removed unused supabaseClient initialization and checks.

--- js/supabase.js
+++ js/supabase.js
@@ -8,15 +8,6 @@ const SUPABASE_URL =
 const SUPABASE_ANON_KEY =
     "sb_publishable_zCzBS1o-NxP6KyZ7JCT53g_1qRd5reQ";
 
-
-let supabaseClient = null;
-
-
-if (
-    SUPABASE_URL !== "YOUR_SUPABASE_URL" &&
-    SUPABASE_ANON_KEY !== "YOUR_SUPABASE_ANON_KEY"
-) {
-
     supabaseClient =
         window.supabase.createClient(
             SUPABASE_URL,
